fc1cc4748d7bbaff50dcf440cd0c1264a4c6d48d
aquilasdjv <49687682+aquilasdjv@users.noreply.github.com>
2026-02-01 12:25:17 +0100

    Add initial Hello_World script and shortcuts configuration

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+# Hello_World.py
+"""
+Docstring for Hello_World
+: This script prints "Hello, World!" to the console.
+@author aquilas djenontin
+""" 
+
+if __name__ == '__main__':
+    print("Hello, World!")
