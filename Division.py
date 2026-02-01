db832cabf1eeb998f4b70e3dfa1a5d2e6222b2e8
aquilasdjv <49687682+aquilasdjv@users.noreply.github.com>
2026-02-01 12:45:10 +0100

    Add Division script for performing division and floor division operations

---
+++ Division.py
@@ -0,0 +1,13 @@
+# Division.py
+"""
+Docstring for Module for performing division operations.
+: This script performs division and floor division on two integers provided as input.
+@author aquilas djenontin
+"""
+
+if __name__ == "__main__":
+    a = int(input().strip())
+    b = int(input().strip())
+
+    print(a / b)
+    print(a // b)
