e25b49af137190a4199944663a5f776a837c3665
aquilasdjv <49687682+aquilasdjv@users.noreply.github.com>
2026-02-01 12:35:31 +0100

    Add Arithmetic_Operators script for basic arithmetic operations

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,15 @@
+# Arithmetic_Operators.py
+"""
+Docstring for Arithmetic_Operators
+: This script performs basic arithmetic operations (addition, subtraction, multiplication)
+    on two integers provided as input.
+@author aquilas djenontin
+"""
+
+if __name__ == '__main__':
+    a = int(input().strip())
+    b = int(input().strip())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
